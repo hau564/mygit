343f1c171d95edb3d20aec5dbb1a1dfc488865a4
hau564 <thanhhau050604@gmail.com>
2024-05-05 12:05:24 +0700

    Update object load from hash

--- src/commands/cat_file.rs
+++ src/commands/cat_file.rs
@@ -3,8 +3,8 @@ use crate::objects::Object;
 pub(crate) fn invoke(pretty_print: bool, object_hash: &str) -> std::io::Result<()> {
     println!("cat-file: pretty_print = {pretty_print}, object = {object_hash}");
     let object = Object::new(object_hash)?;
-    println!("{}",object.kind);
-    println!("{}",object.size);
+    // println!("{}",object.kind);
+    // println!("{}",object.size);
     println!("{}",object.content);
     Ok(())
 }
--- src/objects.rs
+++ src/objects.rs
@@ -2,13 +2,10 @@ use std::io::BufReader;
 use std::io::prelude::*;
 use flate2::read::ZlibDecoder;
 
-// pub(crate) mod kind;
-// use kind::Kind;
-
 pub(crate) struct Object {
+    pub size: i64,
     pub kind: String,
-    pub content: String,
-    pub size: i32
+    pub content: String
 }
 
 impl Object {
@@ -16,36 +13,29 @@ impl Object {
         let f = std::fs::File::open(format!(".git/objects/{}/{}", &object_hash[..2], &object_hash[2..]))?;
         let z = ZlibDecoder::new(f);
         let mut z = BufReader::new(z);
-        let mut bytes = Vec::new();
-        z.read_to_end(&mut bytes)?;
         
-        let mut kind_bytes = Vec::new();
-        let mut size_bytes = Vec::new();
-        let mut content_bytes = Vec::new();
-        let mut started = false;
-        let mut space = false;
-        for i in &bytes {
-            if started {
-                if *i < 254 {content_bytes.push(*i);}
-            }
-            else {
-                if *i == 0 {started = true;}
-                else {
-                    if (*i) == b' ' {space = true; continue;}
-                    if space == false {
-                        kind_bytes.push(*i);                        
-                    }
-                    else {
-                        size_bytes.push(*i);
-                    }
-                }
+        let mut kind = Vec::new();
+        z.read_until(b' ', &mut kind)?;
+        kind.pop();
+        let kind = String::from_utf8(kind).expect("Convert bytes to string");
+
+        let mut size = Vec::new();
+        z.read_until(0, &mut size)?;
+        size.pop();
+        let size = String::from_utf8(size).expect("Convert bytes to string");
+        let size = size.parse::<i64>().unwrap();
+
+        let mut content = Vec::new();
+        z.read_to_end(&mut content)?;
+        let mut content_fix = Vec::new();
+        for i in &content {
+            let c = char::from(*i);
+            if c.is_ascii() {
+                content_fix.push(*i);
             }
         }
+        let content = String::from_utf8(content_fix).expect("Convert content bytes to string");
 
-        let kind = String::from_utf8(kind_bytes).expect("Convert bytes to string");
-        let content = String::from_utf8(content_bytes).expect("Convert bytes to string");
-        let size = String::from_utf8(size_bytes).expect("Convert bytes to string");
-        let size = size.parse::<i32>().unwrap();
-        Ok(Object{kind: kind, content: content, size: size})
+        Ok(Object{kind: kind, size: size, content: content})
     }
 }
